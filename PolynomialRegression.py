3b1a4d99cab88a3d3caa3de330e4ffb8315b166e
zipper112 <zipper112@163.com>
2020-11-27 15:22:26 +0800

    多项式回归

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -6,36 +6,36 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from ModelSelection import trainTestSplit
-from Scaler import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import KFold
 
-
+# -----数据生成-----
 np.random.seed(666)
-X = np.arange(-5, 15, 0.2).reshape(-1, 1)
-y = X * X + 5 + (np.random.random(size=X.shape) - 0.5) * 50
-poly = PolynomialFeatures(degree=15)
-poly.fit(X)
-X = poly.transform(X)
-scaler = StandardScaler()
-scaler.fit(X)
-X = scaler.transForm(X)
-scaler.fit(y)
-y = scaler.transForm(y)
+X = np.arange(-5, 15, 0.1).reshape(-1, 1)
+y = X * X + 5 + (np.random.random(size=X.shape) - 0.5) * np.random.randint(low=50, high=100, size=X.shape)
+
+# -----学习曲线绘制-----
 
 
 def showLearningCurve(data: np.array, target:np.array, train_ratio: float=0.7):
+    poly = Pipeline(
+        [("poly", PolynomialFeatures(degree=2)),
+         ("scaler", StandardScaler()),
+         ("liner", LinearRegression())]
+    )
     X_train, X_test, y_train, y_test = trainTestSplit(data, target, train_ratio=train_ratio)
 
     train_score = []
     test_score = []
-    for i in range(len(X_train)):
-        liner = LinearRegression()
-        liner.fit(X_train[:i + 1], y_train[: i + 1])
-        train_score.append(liner.score(X_train[: i + 1], y_train[: i + 1]) * 50)
-        test_score.append(liner.score(X_test, y_test) * 50)
+    for i in range(1, len(X_train) + 1):
+        poly.fit(X_train[:i], y_train[: i])
+        train_score.append(poly.score(X_train[: i], y_train[: i]))
+        test_score.append(poly.score(X_test, y_test))
 
     plt.plot([i + 1 for i in range(len(train_score))], train_score, label='train')
     plt.plot([i + 1 for i in range(len(train_score))], test_score, label='test')
     plt.legend()
-    plt.ylim(40, 50)
+    plt.ylim(-3, 1)
     plt.show()
-# 这里是一个测试
